257db94dd6cabe021337037644189c47029b7452
JuHn's space <ekaqls9607@gmail.com>
2025-09-20 17:58:42 +0900

    Update CheckPath.java

--- code-examples/chap01_1/CheckPath.java
+++ code-examples/chap01_1/CheckPath.java
@@ -1,5 +1,11 @@
-<p align="right"><a href="../readme.md">🏠 Back to Index</a></p>
-<a id="top"></a>
+/*
+ * Docs(해당 문제 MD): https://github.com/<YOUR_USER>/<YOUR_REPO>/blob/main/MD-File/04-2-loops-for-while-do-while.md#-7-while%EB%AC%B8%EA%B3%BC-scanner%EB%A1%9C-%EC%9E%85%EB%A0%A5-%EA%B8%B0%EB%8A%A5
+ * Index(README):     https://github.com/<YOUR_USER>/<YOUR_REPO>/blob/main/readme.md
+ *
+ * VS Code에서 바로 열기(선택):
+ *  - README: vscode://file/C:/PJH-JavaStudy/정리/readme.md
+ *  - README: file:///C:/PJH-JavaStudy/정리/readme.md
+ */
 package chap01_1;
 
 public class CheckPath {
