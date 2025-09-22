dbb48023af3c741f3e51b47fd29f228be87bbab1
ekaqls9607-cloud <ekaqls9607@gmail.com>
2025-09-22 13:33:25 +0900

    dd.

---
+++ code-examples/chap00/VarExample1.java
@@ -0,0 +1,14 @@
+package chap00;
+
+public class VarExample1 {
+    public static void main(String[] args) {
+        String x = getData();
+        var y = getData();
+    }
+
+    public static String getData(){
+        return "홍길동";
+    }
+
+    
+}
